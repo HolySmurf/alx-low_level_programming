f7525202a8cdf7d436d3ae6d552566830396e91a
Eunice Oladapo <euniceoladapo60@gmail.com>
2022-06-02 14:48:25 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - prints onto screen
  *
- * Return - zero
+ * Return: zero
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+ printf("with proper grammar, but the outcome is a piece of art,\n");
+ return (0);
 }
